2e7b57bcccc3ff0ccd6dc750a28050a5442f4ed6
tabernaculochurch26 <midiatabernaculochurch26@gmail.com>
2026-08-20 19:56:32 -0400

    Atualiza site, fontes e links

--- links.js
+++ links.js
@@ -7,7 +7,8 @@
 */
 
 window.CHURCH_LINKS = {
-  instagram: "https://www.instagram.com/midiatabernaculochurch/",
+  instagram: "https://www.instagram.com/tabernaculochurchUSA/",
   facebook: "https://www.facebook.com/tabernaculochurch3/",
   pictime: "https://tabernaculochurch.pic-time.com/client/",
+  google: "https://www.google.com/maps/place/Tabernáculo+Church/@40.712776,-74.005974,15z/data=!4m2!3m1!1s0x0:0x0?hl=pt-BR",
 };
